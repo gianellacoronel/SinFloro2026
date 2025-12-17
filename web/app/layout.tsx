4c074ce78069e57a1485ba19a872c658b5ba62a5
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2025-12-16 21:35:25 -0500

    feat: update generateMetadata - miniapp

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -20,6 +20,27 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    other: {
+      "fc:miniapp": JSON.stringify({
+        version: "next",
+        imageUrl: "https://sin-floro-2026.vercel.app/hero-image.png",
+        button: {
+          title: `Predict the 2026 Winner`,
+          action: {
+            type: "predict_2026_winner",
+            name: "Sin Floro 2026",
+            url: "https://sin-floro-2026.vercel.app",
+            splashImageUrl: "https://sin-floro-2026.vercel.app/app-icon.png",
+            splashBackgroundColor: "#ffe3aa",
+          },
+        },
+      }),
+    },
+  };
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
