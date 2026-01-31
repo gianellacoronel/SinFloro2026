e8b9373020a60e500c73cfa9c9d468a4905a89d6
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-31 12:19:31 -0500

    Revert "feat: update transactions for faucet"
    This reverts commit 3277cc23b06a14a0559e9a745a4f104f9d99cd07.

--- web/components/market/faucet-promo.tsx
+++ web/components/market/faucet-promo.tsx
@@ -6,40 +6,27 @@ import {
 } from "@/components/custom/monopoly-card";
 import { Coins } from "lucide-react";
 import { MonopolyButton } from "../custom/monopoly-button";
-import {
-  useAccount,
-  useWaitForTransactionReceipt,
-  useWriteContract,
-} from "wagmi";
+import { useAccount, useSendCalls } from "wagmi";
 import {
   INTITOKEN_ABI,
   INTITOKEN_ADDRESS,
+  SIN_FLORO_ADDRESS,
 } from "@/lib/constants/contracts";
 import { Spinner } from "../ui/spinner";
 import { toast } from "sonner";
+import { encodeFunctionData } from "viem";
+import { base } from "viem/chains";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
-import { useEffect } from "react";
 
 export function FaucetPromo() {
   const { address, isConnected } = useAccount();
-  const { writeContract, data: hash, isPending: isWriting } = useWriteContract();
-  const { isLoading: isConfirming, isSuccess: isConfirmed } =
-    useWaitForTransactionReceipt({
-      hash,
-    });
+  const { sendCalls, isPending, isSuccess } = useSendCalls();
 
   const bettor = useQuery(api.bettors.getBettorByWalletAddress, {
     walletAddress: address || "",
   });
   const createBettor = useMutation(api.bettors.createBettor);
-
-  useEffect(() => {
-    if (isConfirmed) {
-      toast.success("Intis reclamados con éxito");
-      createBettor({ walletAddress: address || "" });
-    }
-  }, [isConfirmed, createBettor, address]);
 
   async function handleClaim() {
     try {
@@ -54,12 +41,33 @@ export function FaucetPromo() {
         return;
       }
 
-      writeContract({
+      const callFaucet = encodeFunctionData({
         abi: INTITOKEN_ABI,
-        address: INTITOKEN_ADDRESS,
         functionName: "faucet",
         args: [],
       });
+
+      const callApprove = encodeFunctionData({
+        abi: INTITOKEN_ABI,
+        functionName: "approve",
+        args: [SIN_FLORO_ADDRESS, BigInt(1000 * 10 ** 18)],
+      });
+
+      sendCalls({
+        calls: [
+          {
+            to: INTITOKEN_ADDRESS,
+            data: callFaucet,
+          },
+          {
+            to: INTITOKEN_ADDRESS,
+            data: callApprove,
+          },
+        ],
+        chainId: base.id,
+      });
+
+      createBettor({ walletAddress: address || "" });
     } catch (error: any) {
       void toast.error(error.message);
     }
@@ -81,10 +89,10 @@ export function FaucetPromo() {
             variant="secondary"
             monopolySize="sm"
             className="shrink-0"
-            disabled={isWriting || isConfirming}
+            disabled={isPending}
             onClick={handleClaim}
           >
-            {isWriting || isConfirming ? (
+            {isPending ? (
               <Spinner className="size-6 text-primary" />
             ) : (
               <div className="flex items-center gap-2">
@@ -93,6 +101,7 @@ export function FaucetPromo() {
               </div>
             )}
           </MonopolyButton>
+          {isSuccess && void toast.success("Intis reclamados con éxito")}
         </div>
       </MonopolyCardContent>
     </MonopolyCard>
